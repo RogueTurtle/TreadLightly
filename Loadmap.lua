b4d6eed9c215a6a4dcd1f5a20b80530f11a2a740
RogueTurtle <66068126+RogueTurtle@users.noreply.github.com>
2022-02-01 17:53:11 +0000

    random shit

--- Loadmap.lua
+++ Loadmap.lua
@@ -11,5 +11,11 @@ end
 local f = io.open("Map.txt", "r")
 print(f:read("*a"))
 print("hi")
-
-
+--TODO create array with parsed data from map file and append it to the 2d array with layering
+local mt = {}          -- create the matrix
+    for i=1,10 do
+      mt[i] = {}     -- create a new row
+      for j=1,10 do
+        mt[i][j] = 0
+      end
+    end
