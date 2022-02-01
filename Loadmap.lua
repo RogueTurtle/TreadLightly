21aaae80fc3764a07a2dc751e4f02bb03d580339
RogueTurtle <66068126+RogueTurtle@users.noreply.github.com>
2022-02-01 17:33:48 +0000

    loads map

---
+++ Loadmap.lua
@@ -0,0 +1,15 @@
+-- Loading map file and checking for collisions
+
+function CheckCollision(x,y)
+    if 1 == 1 then
+        print("Fuck Lua")
+    end
+    local colCheck = false
+    return colCheck
+end
+
+local f = io.open("Map.txt", "r")
+print(f:read("*a"))
+print("hi")
+
+
