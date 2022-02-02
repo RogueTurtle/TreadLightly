7f8bc60764d9e2a45fb2cc4251d2dcbffab66522
szarik010 <84450794+szarik010@users.noreply.github.com>
2022-02-02 15:56:40 +0000

    Update main.lua
    Added strafing and improved movement code

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 require("keyboardInput")
---require("Loadmap")
 
 local player = display.newImageRect("arrow.png", 32, 32)
 player.x = display.contentCenterX
@@ -10,11 +9,28 @@ local function movePlayer(speed)
     player.y = player.y - math.floor(math.cos(math.rad(player.rotation))*speed)
 end
 
+local function strafe(speed, dir)
+    if dir == "left" then
+        player.rotation = player.rotation - 90
+        movePlayer(speed)
+        player.rotation = player.rotation + 90
+    elseif dir == "right" then
+        player.rotation = player.rotation + 90
+        movePlayer(speed)
+        player.rotation = player.rotation - 90
+    end
+end
+
 local function gameLoop()
-    if keys.w then
+    if keys.w or keys.up then
         movePlayer(16)
-    elseif keys.s then
+    elseif keys.s or keys.down then
         movePlayer(-16)
+    end
+    if keys.a then
+        strafe(16, "left")
+    elseif keys.d then
+        strafe (16, "right")
     end
     if keys.left then
         player.rotation = player.rotation-5
