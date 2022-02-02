6fa6e176fa5a8c2480ec4bf2b45526712236505a
szarik010 <84450794+szarik010@users.noreply.github.com>
2022-02-02 12:31:44 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local unitSize = 10
+local unitSize = 20
 
 
 local player = {
@@ -7,10 +7,12 @@ local player = {
     rotation = 0
 }
 
+local boxSize = 50
+
 local walls = {}
-for i = 0, 9 do
+for i = 0, boxSize do
     walls[i] = {}
-    for j = 0, 9 do
+    for j = 0, boxSize do
         walls[i][j] = 0
     end
 end
@@ -20,8 +22,8 @@ local function addWall(x, y)
 end
 
 local function drawWalls()
-    for i = 0, walls.length do
-        for j = 0, walls[i].length do
+    for i = 0, #walls do
+        for j = 0, #walls[i] do
             if (walls[i][j]) == 1 then
                 display.newRect(i*unitSize, j*unitSize, unitSize, unitSize)
             end
@@ -29,9 +31,21 @@ local function drawWalls()
     end
 end
 
-for i = 0, 9 do
+for i = 0, boxSize do
     addWall(0, i)
     addWall(i, 0)
-    addWall(9, i)
-    addWall(i, 9)
+    addWall(boxSize, i)
+    addWall(i, boxSize)
 end
+
+drawWalls()
+
+
+local function onKeyEvent(event)
+    print("key: " .. event.keyName .. " phase: " .. event.phase)
+    return false
+end
+
+Runtime:addEventListener("key", onKeyEvent)
+
+display.newRect(0, 0, 100, 100)
